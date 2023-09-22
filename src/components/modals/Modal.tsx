0b650343efd225052e95aa899a4868be8a9a48a9
Austin Thaldorf-Huelsbeck <75189212+austinthaldorfhuelsbeck@users.noreply.github.com>
2023-09-21 18:49:53 -0700

    modal handler with state

--- src/components/modals/Modal.tsx
+++ src/components/modals/Modal.tsx
@@ -17,37 +17,28 @@ export const Modal: React.FC<ModalFormProps> = ({
 
 	// event listeners
 	const onEscape = (e: React.KeyboardEvent<HTMLDialogElement>) => {
+		e.preventDefault()
 		if (e.key === "Escape") closeModal()
 	}
-
-	const onCancel = React.useCallback(() => closeModal(), [closeModal])
-	const onClick = React.useCallback(
-		(e: MouseEvent) => {
-			const { current: el } = modalRef
-			if (e.target === el) closeModal()
-		},
-		[closeModal],
-	)
-	const onAnimEnd = React.useCallback(() => {
-		const ref = modalRef
-		if (ref.current && !isOpen) ref.current.close()
-	}, [isOpen])
+	const onClick = (e: React.MouseEvent<HTMLDialogElement>) => {
+		e.preventDefault()
+		if (e.target === modalRef.current) closeModal()
+	}
 
 	// open and close when isOpen changes
 	React.useEffect(() => {
 		const ref = modalRef
-		if (ref.current && isOpen) ref.current.showModal()
+		if (ref.current) {
+			if (isOpen) {
+				ref.current.showModal()
+			} else {
+				ref.current.close()
+			}
+		}
 	}, [isOpen])
 
 	return (
-		<dialog
-			ref={modalRef}
-			onClose={closeModal}
-			onKeyDown={onEscape}
-			onCancel={onCancel}
-			onClick={() => onClick}
-			onAnimationEnd={onAnimEnd}
-		>
+		<dialog ref={modalRef} onKeyDown={onEscape} onClick={onClick}>
 			{children}
 		</dialog>
 	)
